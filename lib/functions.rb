7edf532a39e574b386e472822cefdcf48e002d1c
Micah Shute <micah.shute@gmail.com>
2019-06-06 15:07:58 -0500

    add merge method to functions

--- lib/functions.rb
+++ lib/functions.rb
@@ -30,4 +30,40 @@ class TxtMine::Functions
 		arr.reduce({}){ |hist, e| hist[e] ||= 0; hist[e] += 1; hist}
 	end
 
+
+	##
+	# Allow a custom merge operation on two objects
+	## rgen = Random.new
+	## coll1 = []
+	## coll2 = []
+	## 10.times { coll1 << OpenStruct.new(num: rgen.rand) }
+	## 10.times { coll2 << OpenStruct.new(num: rgen.rand) }
+	## coll1.sort!{ |a,b| a.num <=> b.num } # => [0.03487909057248051, 0.09917103700673668, 0.268788128873637, 0.4709475457157205, 0.6440975993991198, 0.6787783519600992, 0.8306938448844432, 0.8400887612063969, 0.9061023061228871, 0.9711913973349926] 
+	## coll2.sort!{ |a,b| a.num <=> b.num }# => [0.0027464589764794045, 0.1437025829513453, 0.1551882024338438, 0.3577303682846722, 0.39922026397604227, 0.6064910551493227, 0.7298715585007346, 0.7556301972235012, 0.7557489060549826, 0.8480248342178041] 
+	## res = TxtMine::Functions.merge(coll1, coll2) do |a, b|
+	##   a.num < b.num
+	## end
+	## res # => [0.0027464589764794045, 0.03487909057248051, 0.09917103700673668, 0.1437025829513453, 0.1551882024338438, 0.268788128873637, 0.3577303682846722, 0.39922026397604227, 0.4709475457157205, 0.6064910551493227, 0.6440975993991198, 0.6787783519600992, 0.7298715585007346, 0.7556301972235012, 0.7557489060549826, 0.8306938448844432, 0.8400887612063969, 0.8480248342178041, 0.9061023061228871, 0.9711913973349926] 
+	def self.merge(left, right)
+		merged_arr = []
+		i, j = 0, 0
+		while i < left.length && j < right.length
+			#puts "Comparing lhs #{left[i]} to rhs #{right[j]}"
+			if yield(left[i], right[j]) # For yield(a,b) have a < b be true to have a go first
+				# See above example
+				#puts "Adding lhs #{left[i]}"
+				merged_arr << left[i]
+				i += 1
+			else
+				#puts "Adding rhs #{right[j]}"
+				merged_arr << right[j]
+				j += 1
+			end
+		end
+		merged_arr.concat right[j, right.length] if i == left.length
+		merged_arr.concat left[i, left.length] if j == right.length
+		#puts merged_arr.to_s
+		merged_arr
+	end
+
 end
